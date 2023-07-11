0e086712e02887f2c7c9e8b8005a42121577fcfa
sm_ayman <sultanmdayman@gmail.com>
2023-07-11 19:43:31 +0600

    solved loading issue

--- src/Routes/PrivateRoute/PrivateRoute.js
+++ src/Routes/PrivateRoute/PrivateRoute.js
@@ -1,14 +1,19 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
+import Spinner from 'react-bootstrap/Spinner';
 
 const PrivateRoute = ({ children }) => {
 
-    const { user } = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
     const location = useLocation();
 
+    if (loading) {
+        return <Spinner animation="grow" />;
+    }
+
     if (!user) {
-        return <Navigate to='/login' state={{from: location}} replace></Navigate>
+        return <Navigate to='/login' state={{ from: location }} replace></Navigate>
     }
     return children;
 };
--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -6,28 +6,35 @@ export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
+
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const providerLogin = (provider) => {
+        setLoading(true);
         return signInWithPopup(auth, provider);
     }
 
     const createUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
     const signIn = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
 
     const logOut = () => {
+        setLoading(true);
         return signOut(auth);
     }
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             console.log('inside auth state change', currentUser);
-            setUser(currentUser)
+            setUser(currentUser);
+            setLoading(false);
         });
 
         return () => {
@@ -36,7 +43,7 @@ const AuthProvider = ({ children }) => {
 
     }, [])
 
-    const authInfo = { user, providerLogin, logOut, createUser, signIn };
+    const authInfo = { user, loading, providerLogin, logOut, createUser, signIn };
 
     return (
         <AuthContext.Provider value={authInfo}>
